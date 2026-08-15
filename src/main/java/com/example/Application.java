5da92c50acb1fd8421b17d44e408fddf4c077aba
safrinasif1112-ai <safrinasif1112@gmail.com>
2026-08-15 10:52:01 +0530

    Set server port to 8081 in Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -3,5 +3,6 @@ package com.example;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Java application is running!");
+        server.port=8081
     }
 }
